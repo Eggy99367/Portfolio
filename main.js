efa1f960fd9ae6d8b2c160737d6b2e980a8917ac
Vincent Chen <vince@Vincents-Mac-mini.local>
2025-05-26 17:35:47 -0700

    Dynamic background and Experiences section added

--- main.js
+++ main.js
@@ -1,3 +1,38 @@
+//typed word changing animation
+const typed = new Typed('#typed', {
+    strings: ['Software Engineer', 'Data Scientist', 'Full Stack Web Developer', 'API Engineer', 'Application Developer'],
+    typeSpeed: 70,
+    backSpeed: 40,
+    backDelay: 2000,
+    loop: true
+});
+
+fetch('content/about_me.txt')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('about-me').innerText = data;
+    })
+    .catch(error => console.error('Error loading the text file:', error));
+
+fetch('content/experiences.json')
+    .then(response => response.json())
+    .then(data => {
+        const experiencesGrid = document.getElementById('experiences-grid');
+        for (const experience of Object.values(data)) {
+            const experienceCard = document.createElement('div');
+            experienceCard.className = 'experience-card';
+            experienceCard.innerHTML = `
+                <h3>${experience.company}</h3>
+                <h4>${experience.title}</h4>
+                <h5>${experience.location}</h5>
+                <h5>${experience.time}</h5>
+                ${experience.description.map(bulletpoint => `<p>• ${bulletpoint}</p>`).join('')}
+                `
+            experiencesGrid.appendChild(experienceCard);
+        }
+    })
+    .catch(error => console.error('Error loading the JSON file:', error));
+
 // Smooth scrolling for navigation links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
@@ -31,4 +66,102 @@ logo.addEventListener('click', (e) => {
         e.preventDefault();
         navLinks.style.display = navLinks.style.display === 'none' ? 'flex' : 'none';
     }
-}); 
+});
+
+tsParticles.load("tsparticles", {
+    fullScreen: {
+        enable: false
+    },
+    background: {
+        color: "#ffffff"
+    },
+    particles: {
+        size: {
+            value: { min: 1, max: 3 }
+        },
+        move: {
+            enable: true,
+            speed: 0.5
+        },
+        links: {
+            enable: true,
+            distance: 150,
+            opacity: 1,
+            width: 1,
+            color: {
+                value: "#000000"
+            },
+            
+        },
+        color: {
+            value: "#000000"
+        },
+        opacity: {
+            value: 0.6
+        },
+        number: {
+            value: 80,
+            density: {
+                enable: true,
+                area: 800
+            }
+        },
+        size: {
+            value: { min: 1, max: 3 }
+        }
+    },
+    interactivity: {
+        events: {
+            onHover: {
+                enable: true,
+                mode: ["grab", "bubble", ""]
+            },
+            resize: true
+        },
+        modes: {
+            light: {
+                area: {
+                    gradient: {
+                        start: {
+                            value: "#bbbbbb"
+                        },
+                        stop: {
+                            value: "#ffffff"
+                        }
+                    },
+                    radius: 500
+                },
+                shadow: {
+                    color: {
+                        value: "#000000"
+                    },
+                    length: 0
+                }
+            },
+            grab: {
+                distance: 160,
+                links: {
+                  opacity: 0.8
+                }
+            },
+            bubble: {
+                distance: 150,
+                size: 7,
+                opacity: 1,
+                color: {
+                    value: "#000000"
+                },
+                links: {
+                    enable: true,
+                    distance: 180,
+                    opacity: 0,
+                    width: 1,
+                    color: {
+                        value: "#000000"
+                    }
+                }
+            }
+        }
+    },
+    detectRetina: true
+});
