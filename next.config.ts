1902fa1c1a0a8cb4e556d546a4775ae3bdb51cd4
Vincent Chen <yinhsuac@uci.edu>
2026-08-03 11:55:16 -0700

    fix: added condition to basePath in nextConfig

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/Portfolio",
+  basePath: process.env.GITHUB_ACTIONS === "true" ? '/Portfolio' : '',
   images: {
     unoptimized: true,
   },
